89272a2549adb73987b3c224381cc6a895997d1f
Sergey Ovodov <altager@users.noreply.github.com>
2019-02-18 03:49:45 +0300

    Some fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,12 @@ set -e
 ./bin/wait-for-it.sh redis:6379 -t 25
 
 if [[ $1 = 'app' ]]; then
-    gunicorn --bind 0.0.0.0:8080 songs_app.main:app
+    gunicorn --bind 0.0.0.0:8080 --workers=2 --log-level=info songs_app.main:app
 fi
 
 if [[ $1 = 'tests' ]]; then
     ./bin/wait-for-it.sh songs_app:8080 -t 25
+    find -name "*.pyc" -delete
     pytest --spec -p no:cacheprovider --tb short -vv .
 fi
 
